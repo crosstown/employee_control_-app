9520b93fdc84e24134e0b1b8ad742d49baf7db34
Fernando Simon <royalplanet2009@gmail.com>
2017-11-20 21:31:37 -0500

    Initial commit after creation

---
+++ src/test/java/com/crosstown/employee_control/EmployeeControlApplicationTests.java
@@ -0,0 +1,16 @@
+package com.crosstown.employee_control;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EmployeeControlApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
